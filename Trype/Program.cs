7265e677d50fb69c5643127d81682fa80a64a68f
Kagaminara Shikatsu <misterarkandias@gmail.com>
2017-01-06 21:49:28 +0100

    Changed "Game1" occurences to "TrypeGame"

--- Trype/Program.cs
+++ Trype/Program.cs
@@ -13,7 +13,7 @@ namespace Trype
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new TrypeGame())
                 game.Run();
         }
     }
